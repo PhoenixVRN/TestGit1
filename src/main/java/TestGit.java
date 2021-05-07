d217597dd14221deee08979b70a8dc3057ac122a
EvgenyiVRN <projectfenixru@gmail.com>
2021-05-07 09:31:47 +0300

    0.2

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,8 +1,13 @@
 public class TestGit {
     public static void main(String[] args) {
 test1();
+test2();
     }
     public static void test1(){
         System.out.println("Test1");
+    }
+
+    public static void test2() {
+        System.out.println("Test2");
     }
 }
